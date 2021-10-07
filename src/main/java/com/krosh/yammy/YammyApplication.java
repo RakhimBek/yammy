702bce3753b10eca501c071daa8950f5eced1b16
Raimbek <rrakhimbekov@gmail.com>
2021-10-07 18:48:33 +0600

    init

---
+++ src/main/java/com/krosh/yammy/YammyApplication.java
@@ -0,0 +1,13 @@
+package com.krosh.yammy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YammyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YammyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krosh/yammy/YammyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krosh.yammy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YammyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
