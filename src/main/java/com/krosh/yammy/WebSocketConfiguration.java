f5c703e06f17d2fb903a4b9cf8766c9ba29003c1
Raimbek <rrakhimbekov@gmail.com>
2021-10-07 20:34:43 +0600

    EchoSocketHandler.java

---
+++ src/main/java/com/krosh/yammy/WebSocketConfiguration.java
@@ -0,0 +1,18 @@
+package com.krosh.yammy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(new EchoSocketHandler(), "/ws/echo")
+				.setAllowedOrigins("*");
+	}
+}
